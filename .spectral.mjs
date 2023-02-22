d5648d48d421eceedd1edd82e4e5ee45cc226f2a
AbdulRaoph Fourteia (Abdul) <raoph40@gmail.com>
2023-02-21 22:28:58 -0500

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4NjA2Ng/spectral.js?branch_slug=master&token=a495aa39-ba3e-41b7-88cf-1b74c04e1870";
+export default { extends: ruleset };
